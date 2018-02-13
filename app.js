875e0db882129df5a5aeec5fcdbfdfafc40bce8a
kimshuye <suntisuk.go@gmail.com>
2018-02-13 14:43:50 +0700

    Product Index View

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressHsb = require('express-handlebars');
 
 var index = require('./routes/index');
 // var users = require('./routes/users');
@@ -11,7 +12,7 @@ var index = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs',expressHsb({defaultLayout:'layout',extname:'.hbs'}));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
